2af49e072059d367ffd937fa7b96e7fe202fb869
Lex Berezhny <lex@damoti.com>
2017-12-04 00:09:09 -0500

    highlighter, first pass

--- lib/annotator.dart
+++ lib/annotator.dart
@@ -1,6 +1,37 @@
 import 'dart:html';
 
 
+Iterable<Node> nextOrParentSibling(Node root) sync* {
+    if (root.nextNode != null) {
+        yield* traverse(root.nextNode);
+    } else {
+        var node = root.parentNode;
+        while (node != null) {
+            if (node.nextNode != null) {
+                yield* traverse(node.nextNode);
+                break;
+            }
+            node = node.parentNode;
+        }
+    }
+}
+
+
+Iterable<Node> traverse(Node root) sync* {
+    window.console.log('start traverse for ${root.nodeName}');
+    if (root.nodeName == 'TRIVE-CLAIM') {
+        yield* nextOrParentSibling(root);
+    } else {
+        yield root;
+        for (var node in root.childNodes) {
+            yield* traverse(node);
+            yield node;
+        }
+        yield* nextOrParentSibling(root);
+    }
+}
+
+
 String getCSSSelectorFromElement(Element node) {
 
     var parts = [];
@@ -39,4 +70,40 @@ String getCSSSelectorFromElement(Element node) {
     }
 
     return parts.join('>');
+}
+
+
+void highlightCurrentSelection() {
+    var sel = window.getSelection();
+    var range = sel.getRangeAt(0);
+    highlightSelection(range.startContainer, range.startOffset, range.toString().length);
+}
+
+
+void highlightSelection(Node start, int startOffset, int selectionLength) {
+    Node node;
+    for (node in traverse(start)) {
+        if (node.nodeType == Node.TEXT_NODE && node.parentNode.nodeName != 'TRIVE_CLAIM') {
+            var length = (node as Text).length;
+            if (startOffset >= length) {
+                startOffset -= length;
+            } else {
+                if (startOffset > 0) {
+                    node = (node as Text).splitText(startOffset);
+                    length = (node as Text).length;
+                    startOffset = 0;
+                }
+                if ((selectionLength < length)) {
+                    (node as Text).splitText(selectionLength);
+                }
+                var trive_claim = document.createElement('trive-claim');
+                node.replaceWith(trive_claim);
+                trive_claim.append(node);
+                selectionLength -= length;
+                if (selectionLength <= 0) {
+                    break;
+                }
+            }
+        }
+    }
 }
